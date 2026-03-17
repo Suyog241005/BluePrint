0d4ea29e46b32aa54b5551bc8ca332534f3ddaba
Suyog241005 <suyoghabbu@gmail.com>
2026-03-17 15:52:27 +0530

    reinitialize

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,10 +1,8 @@
-import express from "express";
-import { fromNodeHeaders, toNodeHandler } from "better-auth/node";
-import { auth } from "./lib/auth";
+import express from "express"
 import cors from "cors"
 
-const app = express();
-const port = 3005;
+const app = express()
+const port = 3005
 
 // Configure CORS middleware
 app.use(
@@ -13,22 +11,8 @@ app.use(
     methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
     credentials: true, // Allow credentials (cookies, authorization headers, etc.)
   })
-);
-
-// app.all("/api/auth/*", toNodeHandler(auth)); // For ExpressJS v4
-app.all("/api/auth/*splat", toNodeHandler(auth)); // For ExpressJS v5 
-
-// Mount express json middleware after Better Auth handler
-// or only apply it to routes that don't interact with Better Auth
-app.use(express.json());
-
-app.get("/api/me", async (req, res) => {
- 	const session = await auth.api.getSession({
-      headers: fromNodeHeaders(req.headers),
-    });
-	return res.json(session);
-});
+)
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
